c78989649ba14a875b48ebfe5def3a27094213a4
Pallavisangral <pallavisangral18@gmail.com>
2023-03-23 13:43:58 +0530

    fetched api key and created sampleoutput

--- src/Components/News.js
+++ src/Components/News.js
@@ -4,9 +4,24 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news component
-        <NewsItem/> 
+      <div className = "container my-3"> 
+        <h2>Indian Express- Latest News</h2> 
+        <div className="row">
+          <div className="col-md-4">
+          <NewsItem title="my title" description="mydesc"/>
+          </div>
+          <div className="col-md-4">
+          <NewsItem title="my title" description="mydesc"/>
+          </div>
+          <div className="col-md-4">
+          <NewsItem title="my title" description="mydesc"/>
+          </div>
+
+        
+        
+        </div>
+
+       
       </div>
     )
   }
